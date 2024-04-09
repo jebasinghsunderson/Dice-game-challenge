d3e75f977025730caecea901beb46780845136ef
jebasinghsunderson <75842626+jebasinghsunderson@users.noreply.github.com>
2024-04-09 12:05:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var random1=(Math.random()*6) +1;
+random1=Math.floor(random1);
+console.log(random1);
+
+if(random1===1){
+    var image=document.querySelector(".player1Dice").getAttribute("src")
+    console.log(image);
+}
+var random2=(Math.random()*6) +1;
+random2=Math.floor(random2);
+console.log(random2);
+
